280619258d35cdd6fb5c0ceb39397e8e9b36cac6
frostming <mianghong@gmail.com>
2016-03-06 11:37:04 +0800

    Inital commit, finished 0 and 1

---
+++ q0/add_num.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/ python
+# -*- coding: utf-8 -*-
+# 第 0000 题：将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。 类似于图中效果
+# @Author: FrostMing
+# @Email: mianghong@gmail.com
+# @Date: 2016/3/6
+
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def add_num(img, num):
+    draw = ImageDraw.Draw(img)
+    width, height = img.size
+    font = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf', width / 4)
+    color = '#ff0000'
+    draw.text((width - width / 4, 0), num, font=font, fill=color)
+    img.save('result.jpg', 'jpeg')
+
+
+if __name__ == '__main__':
+    image = Image.open('image.jpg')
+    add_num(image, '99')
---
+++ q1/gen_codes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/ python
+# -*- coding: utf-8 -*-
+# 第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）？
+# @Author: FrostMing
+# @Email: mianghong@gmail.com
+# @Date: 2016/3/6 0006
+
+import string
+import random
+import os
+
+letters = string.letters + string.digits
+
+
+def gen_codes(length):
+    result = ''.join(random.sample(letters, length))
+    return result
+
+
+if __name__ == '__main__':
+    result = open('result.txt', 'w')
+    for i in range(200):
+        result.write(gen_codes(8)+os.linesep)
+    result.close()
